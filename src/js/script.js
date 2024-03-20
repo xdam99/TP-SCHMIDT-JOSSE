4392468fbf7e3bff9ead33c85964b8bf5bfd4855
Damien J. <damienjss02100@gmail.com>
2024-03-20 14:23:54 +0100

    Ajout du header en fixed via JS

--- src/js/script.js
+++ src/js/script.js
@@ -2,4 +2,9 @@ let header = document.querySelector('#header')
 
 window.addEventListener('scroll', ()=>{
     console.log(scrollY)
+    if(scrollY>=100){
+        header.style.position = "fixed"
+    } else {
+        header.style.position = "relative"
+    }
 })
