4c4597676adda6ecbeceebc867d76a76c8d40d48
Damien J. <damienjss02100@gmail.com>
2024-03-20 17:03:16 +0100

    Animation header

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,19 @@
 console.log("LAURA SCHMIDT - DAMIEN JOSSE | 3WA DFS")
 
 let header = document.querySelector('#header')
+let logoHeader = document.querySelector('#logo')
 
 window.addEventListener('scroll', ()=>{
     console.log(scrollY)
-    if(scrollY>=100){
+    if(scrollY>=600){
         header.style.position = "fixed"
+        header.style.height = "75px"
+        logoHeader.style.width = "75px"
+        logoHeader.style.height = "50px"
     } else {
         header.style.position = "relative"
+        header.style.height = "100px"
+        logoHeader.style.width = "100px"
+        logoHeader.style.height = "75px"
     }
 })
