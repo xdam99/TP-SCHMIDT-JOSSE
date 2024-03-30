4707982f6a49646c67dea6dc6d0bafe11ba02c6e
Laura Schmidt <lauramarie.sdt@gmail.com>
2024-03-30 16:09:16 +0100

    lien json js

--- src/js/store.js
+++ src/js/store.js
@@ -1,4 +1,9 @@
-import products from '../json/produits.json';
-
-// Utilisez les données JSON ici
-console.log(products);
+fetch('./src/json/produits.json')
+    .then(response => response.json())
+    .then(data => {
+        console.log("Données JSON chargées avec succès :");
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Une erreur s\'est produite lors du chargement du fichier JSON:', error);
+    });
