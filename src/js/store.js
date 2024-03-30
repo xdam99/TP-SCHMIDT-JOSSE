c01f82f58288fc3f8418d456f9bb9ec1fac4cf42
Damien J. <damienjss02100@gmail.com>
2024-03-30 15:34:36 +0100

    ajout tableau produits

--- src/js/store.js
+++ src/js/store.js
@@ -1,19 +1,50 @@
 let produits = [
     {
-    "nom":"Bouillon",
-    "prenom":"Godefroy",
-    "age":33,
-    "activite":["tennis","informatique","foot"],
-    "adresse":{"commune":"Schaerbeek","cp":"1030"}
-    "actif":true,
+        "nom":"xplorer xbox",
+        "plateforme":"xbox",
+        "prix":150,
+        "filaire":true,
     }
     ,
     {
-    "nom":"L'Hermitte",
-    "prenom":"Pierre",
-    "age":30,
-    "activite":["balle pelote","ski","lecture","promebade"],
-    "adresse":{"commune":"Bruxelles","cp":"1000"},
-    "actif":true
+        "nom":"les paul wii",
+        "plateforme":"wii",
+        "prix":70,
+        "filaire":false,
     }
+    ,
+    {
+        "nom":"les paul xbox",
+        "plateforme":"xbox",
+        "prix":70,
+        "filaire":false,
+    }
+    ,
+    {
+        "nom":"les paul playstation",
+        "plateforme":"playstation",
+        "prix":70,
+        "filaire":false,
+    }
+    ,
+    {
+        "nom":"stratocoaster wii",
+        "plateforme":"wii",
+        "prix":50,
+        "filaire":false,
+    }
+    ,
+    {
+        "nom":"stratocoaster xbox",
+        "plateforme":"xbox",
+        "prix":50,
+        "filaire":false,
+    }
+    ,
+    {
+        "nom":"stratocoaster playstation",
+        "plateforme":"playstation",
+        "prix":50,
+        "filaire":false,
+    }   
 ];
