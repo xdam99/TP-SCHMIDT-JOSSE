cc09ac3f7adf4d24449930bd2edb7101e870f1fb
Damien J. <damienjss02100@gmail.com>
2024-04-07 17:36:33 +0200

    ajout fonction

--- src/js/store.js
+++ src/js/store.js
@@ -1,9 +1,22 @@
+let cards = document.querySelector('#cards')
+
+
 fetch('./src/json/produits.json')
     .then(response => response.json())
     .then(data => {
         console.log("Données JSON chargées avec succès :");
         console.log(data);
+        for(let i=0; i < data.length ; i++){
+            const titleContainer = document.createElement("div");
+            titleContainer.setAttribute("id", "card"+i)
+            cards.appendChild(titleContainer)
+            
+            const titleElement = document.createElement('h2');
+            titleElement.textContent = data[i].nom;
+            titleContainer.appendChild(titleElement);
+        }
     })
     .catch(error => {
         console.error('Une erreur s\'est produite lors du chargement du fichier JSON:', error);
     });
+
